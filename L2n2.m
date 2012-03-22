a2f0aa53155f6c402f63a6adae4837b3711d9fb4
overlord_tm <andraz@vrhovec.si>
2012-03-22 23:12:50 +0100

    backup!

---
+++ L2n2.m
@@ -0,0 +1,29 @@
+## Copyright (C) 2012 Andraz Vrhovec
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## ./demo
+
+## Author: Andraz Vrhovec <az@zeus>
+## Created: 2012-03-22
+
+function [ alpha, pi, p ] = L2n2(alpha, beta, pi, p)
+	
+	if( beta == 0 ) %reward
+		pi
+
+	
+
+endfunction
