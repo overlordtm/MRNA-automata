1752a53da0c53fc74876999164d46ad460e07feb
overlord_tm <andraz@vrhovec.si>
2012-03-22 21:19:47 +0100

    nekje je treba zacet

---
+++ demo.m
@@ -0,0 +1,34 @@
+## Copyright (C) 2012 Andraz Vrhovec
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## ./demo
+
+## Author: Andraz Vrhovec <az@zeus>
+## Created: 2012-03-22
+
+function [ ret ] = demo ()
+	PCA()
+endfunction
+
+function [ r ] = PCA()
+
+	if (rand() > 0.5)
+		r = [0, 1];
+	else
+		r = [1, 0];
+	endif
+
+endfunction
